8c360583fb8f31db8235ace5785e566fbce439c6
alexcarmo <alex.carmo@arrivati.com.br>
2021-11-21 01:18:09 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import multer from "multer";
+
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategory/ListCategoriesController";
+
+const categorieRoutes = Router();
+
+const upload = multer({ dest: "./tmp" });
+
+const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
+
+categorieRoutes.post("/", createCategoryController.handle);
+
+categorieRoutes.get("/", listCategoriesController.handle);
+
+categorieRoutes.post(
+    "/import",
+    upload.single("file"),
+    importCategoryController.handle
+);
+
+export { categorieRoutes };
