270162097a4033c181ee1a76a8b725a47f6eceac
alexcarmo <alex.carmo@arrivati.com.br>
2021-11-28 21:49:44 -0300

    Database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -13,12 +13,12 @@ getConnectionOptions().then((options) => {
         ...options,
     });
 }); */
-
-export default async (host = "database_ignite"): Promise<Connection> => {
+// export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
+            // host: process.env.NODE_ENV === "test" ? "localhost" : host,
             database:
                 process.env.NODE_ENV === "test"
                     ? "rentxtest"
